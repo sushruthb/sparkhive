e04b0194d515a011238822d9125fa4326041daed
HG <bharathhg@gmail.com>
2022-09-26 18:38:28 +0530

    Updated spark file

--- src/main/scala/HiveOperations.scala
+++ src/main/scala/HiveOperations.scala
@@ -1,22 +1,75 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
+
+import java.io.File
+
+case class Record(key: Int, value: String)
 
 object HiveOperations {
   def main(args: Array[String]): Unit ={
+    val warehouseLocation = new File("hdfs:/warehouse/tablespace/managed/hive/dc25qacand_audit.db").getAbsolutePath
 
-    val spark=SparkSession.builder().appName("Hive Ops").enableHiveSupport().getOrCreate()
+    val spark = SparkSession
+      .builder()
+      .appName("Spark Hive Example")
+      .config("spark.sql.warehouse.dir", warehouseLocation)
+      .enableHiveSupport()
+      .getOrCreate()
 
-    val dbName="default"
+    import spark.implicits._
+    import spark.sql
 
-    val tableName="test"
-    spark.sql("use "+dbName )
-    spark.sql("SET hive.exec.dynamic.partition=true")
-    spark.sql("SET hive.exec.dynamic.partition.mode=nonstrict")
-    spark.sql("SET hive.exec.max.dynamic.partitions.pernode=400")
 
-    val sqlString="create table if not exists " + dbName + "." + tableName + "(empid String,ename String, designation String,dept String)"
-    spark.sql(sqlString);
+    sql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING) USING hive")
+    sql("LOAD DATA LOCAL INPATH 'src/main/resources/kv1.txt' INTO TABLE src")
 
-    spark.close()
+    // Queries are expressed in HiveQL
+    sql("SELECT * FROM src").show()
+
+    // Queries are expressed in HiveQL
+    sql("SELECT * FROM src").show()
+
+    // Aggregation queries are also supported.
+    sql("SELECT COUNT(*) FROM src").show()
+
+    // The results of SQL queries are themselves DataFrames and support all normal functions.
+    val sqlDF = sql("SELECT key, value FROM src WHERE key < 10 ORDER BY key")
+
+    // The items in DataFrames are of type Row, which allows you to access each column by ordinal.
+    val stringsDS = sqlDF.map {
+      case Row(key: Int, value: String) => s"Key: $key, Value: $value"
+    }
+    stringsDS.show()
+
+    // You can also use DataFrames to create temporary views within a SparkSession.
+    val recordsDF = spark.createDataFrame((1 to 100).map(i => Record(i, s"val_$i")))
+    recordsDF.createOrReplaceTempView("records")
+
+    // Queries can then join DataFrame data with data stored in Hive.
+    sql("SELECT * FROM records r JOIN src s ON r.key = s.key").show()
+
+    sql("CREATE TABLE hive_records(key int, value string) STORED AS PARQUET")
+    // Save DataFrame to the Hive managed table
+    val df = spark.table("src")
+    df.write.mode(SaveMode.Overwrite).saveAsTable("hive_records")
+    // After insertion, the Hive managed table has data now
+    sql("SELECT * FROM hive_records").show()
+
+    val dataDir = "/tmp/parquet_data"
+    spark.range(10).write.parquet(dataDir)
+    // Create a Hive external Parquet table
+    sql(s"CREATE EXTERNAL TABLE hive_bigints(id bigint) STORED AS PARQUET LOCATION '$dataDir'")
+    // The Hive external table should already have data
+    sql("SELECT * FROM hive_bigints").show()
+
+    // Turn on flag for Hive Dynamic Partitioning
+    spark.sqlContext.setConf("hive.exec.dynamic.partition", "true")
+    spark.sqlContext.setConf("hive.exec.dynamic.partition.mode", "nonstrict")
+    // Create a Hive partitioned table using DataFrame API
+    df.write.partitionBy("key").format("hive").saveAsTable("hive_part_tbl")
+    // Partitioned column `key` will be moved to the end of the schema.
+    sql("SELECT * FROM hive_part_tbl").show()
+    spark.stop()
+
   }
 
 }
